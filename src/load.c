5211e20471421bda11a1d12bc769257ca83a5b3b
Jaakko Saarinen <pod@fea.st>
2022-03-26 11:10:49 +0200

    loading into map

--- src/load.c
+++ src/load.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:00:22 by jsaarine          #+#    #+#             */
-/*   Updated: 2022/03/26 11:09:16 by jsaarine         ###   ########.fr       */
+/*   Updated: 2022/03/26 11:10:41 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,40 +25,35 @@ int load_map(char *filename, t_vec *map)
 
 	fd = open(filename, O_RDONLY);
 	vec_new(map, BUFF_SIZE * 2, sizeof(t_vec));
-
 	r = 1;
 	while(r)
 	{
 		vec_new(&linevec, BUFF_SIZE * 2, sizeof(int));
 		r = get_next_line(fd, &line);
-		//ft_putstr(line);
-		//ft_putchar('\n');
 		words = ft_strsplit(line, ' ');
 		while (*words != 0)
 		{
 			k = ft_atoi(*words);
 			vec_push(&linevec, &k);
-			//ft_putstr(*words);
-			//ft_putchar(' ');
 			words++;
 		}
-		//ft_putchar('\n');
 		vec_push(map, &linevec);
 	}
-r = 0;
-while (r < (int)map->len)
-{
-	temp = vec_get(map, r);
-	k = 0;
-	while (k < (int)temp->len)
+	// print it out to make sure it worked
+	r = 0;
+	while (r < (int)map->len)
 	{
-		ft_putnbr(*(int *)vec_get(temp, k));
-		ft_putchar(' ');
-		k++;
+		temp = vec_get(map, r);
+		k = 0;
+		while (k < (int)temp->len)
+		{
+			ft_putnbr(*(int *)vec_get(temp, k));
+			ft_putchar(' ');
+			k++;
+		}
+			ft_putchar('\n');
+		r++;
 	}
-		ft_putchar('\n');
-	r++;
-}
 	close(fd);
 	return (1);
 }
